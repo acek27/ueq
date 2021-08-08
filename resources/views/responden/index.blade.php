385257233a1019c46c714a22756d1ede48289338
Razak Syaiful Rochman <razak.syaiful.r@gmail.com>
2021-08-08 20:57:10 +0700

    region

--- resources/views/responden/index.blade.php
+++ resources/views/responden/index.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.master')
+@push('css')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
+
+@endpush
 @section('content')
     @include('layouts._nav')
 
@@ -78,10 +82,49 @@
                                 <div class="col-md-6">
                                     <div class="contact-form mt-45">
                                         <label>Asal Wilayah Kendaraan</label>
-                                        <select class="form-control border border-primary" name="location"
+                                        <select class="form-control select2" name="location"
                                                 id="location" required>
                                             <option value="">--pilih wilayah--</option>
-                                            <option value="jember">Jember</option>
+                                            <option value="bangkalan">Kabupaten Bangkalan</option>
+                                            <option value="banyuwangi">Kabupaten Banyuwangi</option>
+                                            <option value="blitar">Kabupaten Blitar</option>
+                                            <option value="bojonegoro">Kabupaten Bojonegoro</option>
+                                            <option value="bondowoso">Kabupaten Bondowoso</option>
+                                            <option value="batu">Kabupaten Batu</option>
+                                            <option value="blitar">Kabupaten Blitar</option>
+                                            <option value="gresik">Kabupaten Gresik</option>
+                                            <option value="jember">Kabupaten Jember</option>
+                                            <option value="jombang">Kabupaten Jombang</option>
+                                            <option value="kediri">Kabupaten Kediri</option>
+                                            <option value="lamongan">Kabupaten Lamongan</option>
+                                            <option value="lumajang">Kabupaten Lumajang</option>
+                                            <option value="madiun">Kabupaten Madiun</option>
+                                            <option value="magetan">Kabupaten Magetan</option>
+                                            <option value="malang">Kabupaten Malang</option>
+                                            <option value="mojokerto">Kabupaten Mojokerto</option>
+                                            <option value="nganjuk">Kabupaten Nganjuk</option>
+                                            <option value="ngawi">Kabupaten Ngawi</option>
+                                            <option value="pacitan">Kabupaten Pacitan</option>
+                                            <option value="pamekasan">Kabupaten Pamekasan</option>
+                                            <option value="pasuruan">Kabupaten Pasuruan</option>
+                                            <option value="ponorogo">Kabupaten Ponorogo</option>
+                                            <option value="probolinggo">Kabupaten Probolinggo</option>
+                                            <option value="sampang">Kabupaten Sampang</option>
+                                            <option value="sidoarjo">Kabupaten Sidoarjo</option>
+                                            <option value="situbondo">Kabupaten Situbondo</option>
+                                            <option value="sumenep">Kabupaten Sumenep</option>
+                                            <option value="trenggalek">Kabupaten Trenggalek</option>
+                                            <option value="tuban">Kabupaten Tuban</option>
+                                            <option value="tulungagung">Kabupaten Tulungagung</option>
+                                            <option value="kota batu">Kota Batu</option>
+                                            <option value="kota blitar">Kota Blitar</option>
+                                            <option value="kota kediri">Kota Kediri</option>
+                                            <option value="kota madiun">Kota Madiun</option>
+                                            <option value="kota malang">Kota Malang</option>
+                                            <option value="kota mojokerto">Kota Mojokerto</option>
+                                            <option value="kota pasuruan">Kota Pasuruan</option>
+                                            <option value="kota probolinggo">Kota Probolinggo</option>
+                                            <option value="kota surabaya">Kota Surabaya</option>
                                         </select>
                                     </div> <!-- contact-form -->
                                 </div>
@@ -99,3 +142,11 @@
         </div> <!-- container -->
     </div> <!-- header hero -->
 @endsection
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('.select2').select2();
+        });
+    </script>
+@endpush
