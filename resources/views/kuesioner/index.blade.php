cd6e88cd7a68e3aac0dfa7c3a06e489c82e02a56
Razak Syaiful Rochman <razak.syaiful.r@gmail.com>
2021-09-02 10:02:28 +0700

    navlist hidden

--- resources/views/kuesioner/index.blade.php
+++ resources/views/kuesioner/index.blade.php
@@ -142,30 +142,26 @@
                                 </ul>
                             </div>
                         @endif
-                        <h4 class="contact-title"><i class="lni-check-box"></i> Form
+                        <h4 class="contact-title mb-20"><i class="lni-check-box"></i> Form
                             <span>Survei pengalaman pengguna.</span>
                         </h4>
 
-                        <div class="stepwizard">
+                        <div class="stepwizard" style="display: none">
                             <div class="stepwizard-row setup-panel">
                                 <div class="stepwizard-step">
                                     <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
-                                    <p>Step 1</p>
                                 </div>
                                 <div class="stepwizard-step">
                                     <a href="#step-2" type="button" class="btn btn-default btn-circle"
                                        disabled="disabled">2</a>
-                                    <p>Step 2</p>
                                 </div>
                                 <div class="stepwizard-step">
                                     <a href="#step-3" type="button" class="btn btn-default btn-circle"
                                        disabled="disabled">3</a>
-                                    <p>Step 3</p>
                                 </div>
                                 <div class="stepwizard-step">
                                     <a href="#step-4" type="button" class="btn btn-default btn-circle"
                                        disabled="disabled">3</a>
-                                    <p>Step 4</p>
                                 </div>
                             </div>
                         </div>
